f934a9f3f5717214d5bb944489e44873b837fae4
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-11 17:01:23 +0330

    refactor: change (Don't have an account?Sign up) section's place

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { useId } from "react";
+import { Link } from "react-router-dom";
 import CustomInput from "../CustomInput/CustomInput";
 import CustomButton from "../CustomButton/CustomButton";
 
@@ -7,28 +8,36 @@ const LoginForm = (): React.ReactNode => {
   const passwordInputId = useId();
 
   return (
-    <form action="" className="space-y-6">
-      <CustomInput
-        id={emailInputId}
-        type="email"
-        label="Email"
-        required
-        onChange={(e) => {}}
-      />
-      <CustomInput
-        id={passwordInputId}
-        type="password"
-        label="password"
-        minLength={8}
-        required
-        onChange={(e) => {}}
-      />
-      <CustomButton
-        title="submit"
-        type="submit"
-        className=" bg-transparent border-2 bg-primary text-light"
-      />
-    </form>
+    <>
+      <form action="" className="space-y-6">
+        <CustomInput
+          id={emailInputId}
+          type="email"
+          label="Email"
+          required
+          onChange={(e) => {}}
+        />
+        <CustomInput
+          id={passwordInputId}
+          type="password"
+          label="password"
+          minLength={8}
+          required
+          onChange={(e) => {}}
+        />
+        <CustomButton
+          title="submit"
+          type="submit"
+          className=" bg-transparent border-2 bg-primary text-light"
+        />
+      </form>
+      <div className="text-lg mb-14 space-x-2 text-center py-4">
+        <span className="text-[#5C4529]">Don't have an account?</span>
+        <Link className="text-blue-400 font-medium" to="/auth/register">
+          Sign up
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Container from "../../components/Container/Container";
 import LoginForm from "../../components/LoginForm/LoginForm";
 
@@ -8,12 +7,6 @@ const LoginPage = (): React.ReactNode => {
       <Container>
         <h1 className="text-center text-5xl font-bold py-6">Login</h1>
         <LoginForm />
-        <div className="text-lg mb-14 space-x-2 text-center py-4">
-          <span className="text-[#5C4529]">Don't have an account?</span>
-          <Link className="text-blue-400 font-medium" to="/auth/register">
-            Sign up
-          </Link>
-        </div>
       </Container>
     </main>
   );
