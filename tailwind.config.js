6ca3e276cc8fa9ca6f46b776eafebfc5b2051769
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-11 16:38:15 +0330

    feat: add colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0d6efd",
+        secondary: "#6c757d",
+        success: "#198754",
+        danger: "#dc3545",
+        warning: "#ffc107",
+        info: "#0dcaf0",
+        light: "#f8f9fa",
+        dark: "#212529",
+      },
+    },
   },
   plugins: [],
 };
