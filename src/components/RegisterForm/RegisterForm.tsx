fd7013aa7c2a67c40ff7839bd024b6c003e2f0af
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-11 17:01:44 +0330

    refactor: change (Already registered?Log in) section's place

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,4 +1,5 @@
 import { useId } from "react";
+import { Link } from "react-router-dom";
 import CustomInput from "../CustomInput/CustomInput";
 import CustomButton from "../CustomButton/CustomButton";
 
@@ -8,35 +9,43 @@ const RegisterForm = (): React.ReactNode => {
   const passwordInputId = useId();
 
   return (
-    <form action="" className="space-y-6">
-      <CustomInput
-        id={nameInputId}
-        label="Name"
-        minLength={3}
-        required
-        onChange={(e) => {}}
-      />
-      <CustomInput
-        id={emailInputId}
-        type="email"
-        label="Email"
-        required
-        onChange={(e) => {}}
-      />
-      <CustomInput
-        id={passwordInputId}
-        type="password"
-        label="password"
-        minLength={8}
-        required
-        onChange={(e) => {}}
-      />
-      <CustomButton
-        title="submit"
-        type="submit"
-        className=" bg-transparent border-2 bg-primary text-light"
-      />
-    </form>
+    <>
+      <form action="" className="space-y-6">
+        <CustomInput
+          id={nameInputId}
+          label="Name"
+          minLength={3}
+          required
+          onChange={(e) => {}}
+        />
+        <CustomInput
+          id={emailInputId}
+          type="email"
+          label="Email"
+          required
+          onChange={(e) => {}}
+        />
+        <CustomInput
+          id={passwordInputId}
+          type="password"
+          label="password"
+          minLength={8}
+          required
+          onChange={(e) => {}}
+        />
+        <CustomButton
+          title="submit"
+          type="submit"
+          className=" bg-transparent border-2 bg-primary text-light"
+        />
+      </form>
+      <div className="text-lg mb-14 space-x-2 text-center py-4">
+        <span className="text-[#5C4529]">Already registered?</span>
+        <Link className="text-blue-400 font-medium" to="/auth/login">
+          Log in
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Container from "../../components/Container/Container";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
@@ -8,12 +7,6 @@ const RegisterPage = (): React.ReactNode => {
       <Container>
         <h1 className="text-center text-5xl font-bold py-6">Register Page</h1>
         <RegisterForm />
-        <div className="text-lg mb-14 space-x-2 text-center py-4">
-          <span className="text-[#5C4529]">Already registered?</span>
-          <Link className="text-blue-400 font-medium" to="/auth/login">
-            Log in
-          </Link>
-        </div>
       </Container>
     </main>
   );
