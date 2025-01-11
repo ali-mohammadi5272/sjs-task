69cc8c2048902cfc6328393a9b134bbe80fa47f3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-11 14:09:36 +0330

    feat: create Register Page

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import Container from "../../components/Container/Container";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+const RegisterPage = (): React.ReactNode => {
+  return (
+    <main>
+      <Container>
+        <h1 className="text-center text-5xl font-bold">Register Page</h1>
+        <RegisterForm />
+        <div className="text-lg mb-14 space-x-2">
+          <span className="text-[#5C4529]">Already registered?</span>
+          <Link className="text-blue-400 font-medium" to="/auth/login">
+            Log in
+          </Link>
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+export default RegisterPage;
