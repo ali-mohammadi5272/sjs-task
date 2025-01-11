95854ca2513b65ed84ad21f03adbc0d564b6e801
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-11 16:44:36 +0330

    style: remove styles

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -8,7 +8,7 @@ const CustomButton: React.FC<CustomButtonPropsType> = ({
 }): React.ReactNode => {
   return (
     <button
-      className={`w-full text-center text-xl bg-transparent border-2 border-primary text-primary hover:bg-primary hover:text-light py-2 rounded-md font-bold ${className}`}
+      className={`w-full text-center text-xl py-2 rounded-md font-bold ${className}`}
       onClick={onClick}
       type={type ? type : "button"}
     >
