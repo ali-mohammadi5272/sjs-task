3c436930069231a07acee79b30f53663c35f14ec
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-11 14:10:24 +0330

    feat: add AthLayout to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,12 @@
 import { RouteObject } from "react-router-dom";
+import AuthLayout from "./layouts/Auth/AuthLayout";
 
-const routes: RouteObject[] = [];
+const routes: RouteObject[] = [
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [],
+  },
+];
 
 export { routes };
