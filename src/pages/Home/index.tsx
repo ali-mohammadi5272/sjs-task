36c38514a68c830a432be1671f8b13cd5d3eab13
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-12 18:07:34 +0330

    feat: create Home Page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Container from "../../components/Container/Container";
+
+const HomePage = (): React.ReactNode => {
+  return (
+    <main>
+      <Container>
+        <h1 className="text-center text-5xl font-bold py-6">Home</h1>
+      </Container>
+    </main>
+  );
+};
+
+export default HomePage;
