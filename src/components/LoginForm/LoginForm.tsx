d648b9bf2b55fa49b4bd87db93a47dea960b741c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-12 16:54:58 +0330

    feat: create formSubmitHandler function

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,15 +1,65 @@
-import { useId } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useId, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import CustomInput from "../CustomInput/CustomInput";
 import CustomButton from "../CustomButton/CustomButton";
+import axios from "../../services/axios";
+import { FormDataType, LoginResponseType } from "./loginForm.type";
+import { toast } from "react-toastify";
+import { messages, tokens } from "../../utils/constants";
+import { setCookie } from "../../utils/helperFunctions";
+import { LoadingContext } from "../../contexts/LoadingProvider/LoadingProvider";
 
 const LoginForm = (): React.ReactNode => {
   const userNameInputId = useId();
   const passwordInputId = useId();
+  const navigate = useNavigate();
+  const { setIsLoading } = useContext(LoadingContext);
+  const [formData, _] = useState<FormDataType>({
+    username: "emilys",
+    password: "emilyspass",
+  });
+
+  const formSubmitHandler = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    const response = await axios.post<LoginResponseType>(
+      "/auth/login",
+      formData
+    );
+
+    if (response.status !== 200) {
+      toast.error(messages.LOGIN.ERR);
+      return;
+    }
+
+    toast.success(messages.LOGIN.SUCCESS);
+
+    setCookie({
+      key: tokens.ACCESS_TOKEN,
+      value: response.data.accessToken,
+      maxAge: 60 * 60,
+      path: "/",
+    });
+
+    setCookie({
+      key: tokens.REFRESH_TOKEN,
+      value: response.data.refreshToken,
+      maxAge: 60 * 60 * 24 * 30,
+      path: "/",
+    });
+
+    navigate("/dashboard", { replace: true });
+
+    setIsLoading(false);
+  };
 
   return (
     <>
-      <form action="" className="space-y-6">
+      <form action="" className="space-y-6" onSubmit={formSubmitHandler}>
         <CustomInput
           id={userNameInputId}
           type="text"
