eb58b9daf10184513ef7f340a81a8255a3bf6455
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-12 10:15:04 +0330

    feat: change page's title

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,10 @@
 import Container from "../../components/Container/Container";
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import usePageTitle from "../../hooks/usePageTitle";
 
 const RegisterPage = (): React.ReactNode => {
+  usePageTitle("Register");
+
   return (
     <main>
       <Container>
