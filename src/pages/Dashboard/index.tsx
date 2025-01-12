f731a007e0db3c04db3bb2ab068b0c6c30177fb5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-12 18:04:15 +0330

    feat: create Dashboard Page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Container from "../../components/Container/Container";
+
+const DashboardPage = (): React.ReactNode => {
+  return (
+    <main>
+      <Container>
+        <h1 className="text-center text-5xl font-bold py-6">Dashboard</h1>
+      </Container>
+    </main>
+  );
+};
+
+export default DashboardPage;
