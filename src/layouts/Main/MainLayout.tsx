1572348ba685aed13fffe9a0291e6db0a34228b5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-12 20:24:11 +0330

    feat: create MailLayout

---
+++ src/layouts/Main/MainLayout.tsx
@@ -0,0 +1,43 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import Container from "../../components/Container/Container";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import axios from "../../services/axios";
+import { getCookie } from "../../utils/helperFunctions";
+import { tokens } from "../../utils/constants";
+import { UserType } from "../../models/user";
+
+const MainLayout = (): React.ReactNode => {
+  const { setUser } = useContext(AuthContext);
+
+  const getUser = async () => {
+    const accessToken: string = getCookie(tokens.ACCESS_TOKEN);
+
+    const response = await axios.get<UserType>(
+      "https://dummyjson.com/user/me",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        withCredentials: true,
+      }
+    );
+    setUser(response.data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
+  );
+};
+
+export default MainLayout;
