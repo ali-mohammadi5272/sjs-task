c0eb6d37852e36aa5abd02852bda0e17dc9201c0
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-13 10:06:24 +0330

    style: form styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -8,8 +8,10 @@ const LoginPage = (): React.ReactNode => {
   return (
     <main>
       <Container>
-        <h1 className="text-center text-5xl font-bold py-6">Login</h1>
-        <LoginForm />
+        <div className="h-screen flex justify-center items-center flex-col">
+          <h1 className="text-center text-5xl font-bold py-6">Login</h1>
+          <LoginForm />
+        </div>
       </Container>
     </main>
   );
