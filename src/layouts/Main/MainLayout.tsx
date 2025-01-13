b2e585f4a29e1bfb570a7be38b78b87a3577b9fb
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-13 09:54:41 +0330

    refactor: use tryCatch for error handling

--- src/layouts/Main/MainLayout.tsx
+++ src/layouts/Main/MainLayout.tsx
@@ -5,26 +5,32 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 import axios from "../../services/axios";
 import { getCookie } from "../../utils/helperFunctions";
-import { tokens } from "../../utils/constants";
+import { messages, tokens } from "../../utils/constants";
 import { UserType } from "../../models/user";
+import { toast } from "react-toastify";
+import { LoadingContext } from "../../contexts/LoadingProvider/LoadingProvider";
 
 const MainLayout = (): React.ReactNode => {
   const { setUser } = useContext(AuthContext);
+  const { setIsLoading } = useContext(LoadingContext);
 
   const getUser = async () => {
     const accessToken: string = getCookie(tokens.ACCESS_TOKEN);
-
-    const response = await axios.get<UserType>(
-      "https://dummyjson.com/user/me",
-      {
+    setIsLoading(true);
+    try {
+      const response = await axios.get<UserType>("/user/me", {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
         withCredentials: true,
+      });
+      if (response.status === 200) {
+        setUser(response.data);
       }
-    );
-    if (response.status === 200) {
-      setUser(response.data);
+    } catch (error) {
+      toast.error(messages.LOGIN.ERR);
+    } finally {
+      setIsLoading(false);
     }
   };
 
