846e7bccb35dc8ac1ab4494c027d4d44e0e82b83
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-13 12:16:09 +0330

    fix: fix ProtectedRoute component bug

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,19 +1,13 @@
-import { useLayoutEffect, useState } from "react";
+import { useState } from "react";
 import { tokens } from "../../utils/constants";
 import { getCookie } from "../../utils/helperFunctions";
 import { Navigate } from "react-router-dom";
 import { ProtectedRoutePropsType } from "./protectedRoute.type";
 
 const ProtectedRoute: React.FC<ProtectedRoutePropsType> = ({ children }) => {
-  const [isLogin, setIsLogin] = useState<boolean>(true);
-
-  useLayoutEffect(() => {
-    const cookie: string = getCookie(tokens.REFRESH_TOKEN);
-
-    if (!cookie) {
-      setIsLogin(false);
-    }
-  }, []);
+  const [isLogin, _] = useState<boolean>(
+    getCookie(tokens.REFRESH_TOKEN) ? true : false
+  );
 
   return isLogin ? children : <Navigate to="/auth/login" />;
 };
