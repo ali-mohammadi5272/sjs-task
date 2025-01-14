a88a9cc08c9e9382716ae4a69e1ae2108b9901ba
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-14 15:59:44 +0330

    refactor: use forwardRef

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,30 +1,31 @@
-import React, { memo } from "react";
+import { forwardRef, memo } from "react";
 import Rate from "../Rate/Rate";
 import { ProductCardPropsType } from "./productCard.type";
 
-const ProductCard: React.FC<ProductCardPropsType> = ({
-  price,
-  rating,
-  title,
-  description,
-}) => {
-  return (
-      <article className="px-2 shadow-lg hover:bg-slate-50 cursor-pointer flex flex-col">
-      <header>
-        <h3 className="text-center font-semibold">{title}</h3>
-      </header>
+// const ProductCard: React.FC<ProductCardPropsType> = ({
+const ProductCard = forwardRef<HTMLElement, ProductCardPropsType>(
+  ({ price, rating, title, description }, ref) => {
+    return (
+      <article
+        className="px-2 shadow-lg hover:bg-slate-50 cursor-pointer flex flex-col"
+        ref={ref}
+      >
+        <header>
+          <h3 className="text-center font-semibold">{title}</h3>
+        </header>
         <div className="py-2 max-h-30 overflow-auto flex-grow">
-        <p className="text-gray-600 text-justify">{description}</p>
-      </div>
-      <hr />
-      <footer className="flex justify-between items-center py-2">
-        <div className="text-gray-600">${price}</div>
-        <div>
-          <Rate rate={rating} />
+          <p className="text-gray-600 text-justify">{description}</p>
         </div>
-      </footer>
-    </article>
-  );
-};
+        <hr />
+        <footer className="flex justify-between items-center py-2">
+          <div className="text-gray-600">${price}</div>
+          <div>
+            <Rate rate={rating} />
+          </div>
+        </footer>
+      </article>
+    );
+  }
+);
 
 export default memo(ProductCard);
