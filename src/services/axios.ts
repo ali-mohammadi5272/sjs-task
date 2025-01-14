46950428805c70541898eeb3b0bdf65f92e79563
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-14 18:55:36 +0330

    feat: refreshing AccessToken after get 401 Err

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,38 @@
 import axios from "axios";
 import { AxiosInstance } from "./../../node_modules/axios/index.d";
+import { GetAccessTokenResponseType } from "../types/axios";
+import { getAccessToken, sessionExpireLogout } from "../utils/helperFunctions";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const { accessToken }: GetAccessTokenResponseType =
+          await getAccessToken(axiosInstance);
+
+        axiosInstance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${accessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        sessionExpireLogout();
+
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
