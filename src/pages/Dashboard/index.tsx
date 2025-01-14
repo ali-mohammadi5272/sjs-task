1f9ff336b597ab6c411afa1fc83af960da2a724d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-14 17:04:47 +0330

    feat: add Products Component to Dashboard Page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import Container from "../../components/Container/Container";
+import Products from "../../components/Products/Products";
 
 const DashboardPage = (): React.ReactNode => {
   return (
     <main>
       <Container>
         <h1 className="text-center text-5xl font-bold py-6">Dashboard</h1>
+        <Products />
       </Container>
     </main>
   );
