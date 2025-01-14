95b22868186ae571f421259486d2189b278eef23
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-14 15:58:25 +0330

    style: ProductCard Component styles

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -9,11 +9,11 @@ const ProductCard: React.FC<ProductCardPropsType> = ({
   description,
 }) => {
   return (
-    <article className="px-2 shadow-lg">
+      <article className="px-2 shadow-lg hover:bg-slate-50 cursor-pointer flex flex-col">
       <header>
         <h3 className="text-center font-semibold">{title}</h3>
       </header>
-      <div className="py-2">
+        <div className="py-2 max-h-30 overflow-auto flex-grow">
         <p className="text-gray-600 text-justify">{description}</p>
       </div>
       <hr />
