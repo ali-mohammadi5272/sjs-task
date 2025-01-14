ce59201478d51084ca5bb89b3133bb6c36ab5b59
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-14 18:01:19 +0330

    feat: add NoContent component

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -85,7 +85,7 @@ const Products = (): React.ReactNode => {
 
   return (
     <>
-      {memoProducts.length && false ? (
+      {memoProducts.length ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
           {memoProducts}
         </div>
